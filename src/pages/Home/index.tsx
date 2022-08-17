de3e3659fd6659f1b1af8450981663121dc5e2bf
Eduarda Carvalho <eduardacarvalho9833@gmail.com>
2022-08-17 09:34:28 -0300

    final project

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,124 @@
+import React, { useEffect, useState } from 'react';
+import {
+  View, 
+  Text, 
+  StyleSheet, 
+  TextInput, 
+  Platform,
+  TouchableOpacity,
+  FlatList,
+  StatusBar,
+} from 'react-native';
+import { Button } from '../../components/Button';
+
+interface SkillData{
+  id: string;
+  name: string;
+}
+
+export default function Home(){
+  const [newSkill, setNewSkill] = useState('');
+  const [mySkills, setMySkills] = useState<SkillData[]>([]);
+  const [ greeting, setGreeting] = useState('');
+
+  function handleAddNewSkill() {
+    const data = {
+      id: String(new Date().getTime()),
+      name: newSkill
+    }
+    setMySkills(oldState => [...oldState, data]);
+  }
+  
+
+  function handleRemoveSkill(id: string){
+    setMySkills(oldState => oldState.filter(
+      skill=> skill.id !== id
+    ));
+  }
+
+  useEffect(() => {
+    const currentHour = new Date().getHours();
+    console.log(currentHour);
+    if(currentHour < 12){
+      setGreeting('Good morning');
+    } else if(currentHour >= 12 && currentHour < 18) {
+      setGreeting('Good afternoon');
+    }else{
+      setGreeting('Good night');
+    }
+  }, [])
+
+  return(
+    <View style={styles.container}>
+      
+      <Text style={styles.title}>Welcome, Eduarda</Text>
+      <Text style={styles.greetings}>
+        {greeting}
+      </Text>
+      <TextInput 
+        style={styles.input}
+        placeholder= 'New skill'
+        placeholderTextColor='#555'
+        onChangeText={setNewSkill}
+      />
+      <Button 
+        title='Add'
+        onPress={handleAddNewSkill}
+      />
+      <Text style={[styles.title, {marginVertical:50}]}>
+        My Skills
+      </Text>
+      
+      <FlatList 
+        data={mySkills}
+        keyExtractor={item => item.id}
+        renderItem={({item}) => (
+          <TouchableOpacity 
+            style={styles.buttonSkill}
+            onPress={() => handleRemoveSkill(item.id)}
+          >
+            <Text style={styles.textSkill}>
+              {item.name}
+            </Text>
+          </TouchableOpacity>
+        )}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: '#121015',
+    padding: 25,
+  },
+  title:{
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  input:{
+    backgroundColor: '#1F1e25',
+    color: '#fff',
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+    marginTop: 30,
+    borderRadius: 7
+  },
+  buttonSkill:{
+    backgroundColor: '#1F1e25',
+    padding: 15,
+    borderRadius: 50,
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+  textSkill:{
+    color: '#fff',
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+  greetings:{
+    color: '#fff',
+  },
+})
